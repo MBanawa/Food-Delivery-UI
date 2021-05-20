647f35020b5a9e6c071162dfef65c1c6292b30a9
MBanawa <mikebanawa25@gmail.com>
2021-05-20 16:37:47 +0800

    create homescreen and recent orders widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_ui/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Food Delivery UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      home: HomeScreen(),
+    );
   }
 }
